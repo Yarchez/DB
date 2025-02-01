253568b6048febc3195ac821d01253b4b54df8e9
Yaroslav Smekhnov <yaroslavsmekhnov@MacBook-Air-Yaroslav.local>
2025-02-01 11:09:37 +0300

    added partition finder sql script

---
+++ partitions_find.sql
@@ -0,0 +1,6 @@
+SELECT p.schemaname, p.tablename,
+p.partitionname, p.partitionboundary,
+c.columnname
+FROM pg_partitions p
+JOIN pg_partition_columns c ON p.tablename = c.tablename
+WHERE p.tablename = 'name_of_your_table'
